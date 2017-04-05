ff735d31c3680c7d62eb1638bf7326906c6733f4
Worlanyo <worlalidamalie@gmail.com>
2017-04-05 11:48:00 +0000

    Update to provisioning script

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-
-# A Ubuntu 14.04 install aimed for Ruby on Rails development
-
-# This box start with a basic Ubuntu 14.04 (trusty) based on phusion/ubuntu-14.04-amd64
-# And it adds the following packages aimed for rails development
-
-# - rbenv and ruby-build
-# - ruby 2.1.2 with bundler
-# - Git 1.9.1
-# - Postgresql 9.3 and SQLite
-# - Node.js
-# - PhantomJs 1.9.7
-# - Qt for Capybara Webkit
-# - ImageMagick
-# - Redis and Memcached
+# Original script on https://github.com/orendon/vagrant-rails
 
 # enable console colors
 #sed -i '1iforce_color_prompt=yes' ~/.bashrc
@@ -22,17 +8,17 @@
 echo 'gem: --no-rdoc --no-ri' >> ~/.gemrc
 
 # essentials
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev libcurl4-openssl-dev curl wget
 
 # SQLite, Git and Node.js
 sudo apt-get install -y libsqlite3-dev git nodejs
 
-# Qt for Capybara Webkit
-sudo apt-get install -y libqtwebkit-dev
+# Qt and xvfb-run for Capybara Webkit
+#sudo apt-get install -y libqtwebkit-dev xvfb
 
-# ImageMagick
-sudo apt-get install -y imagemagick
+# ImageMagick and Rmagick
+sudo apt-get install -y imagemagick libmagickwand-dev
 
 # Postgres
 sudo apt-get install -y postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3
@@ -41,7 +27,7 @@ sudo apt-get install -y postgresql-9.3 postgresql-server-dev-9.3 postgresql-cont
 sudo apt-get install -y memcached
 
 # Redis
-#sudo apt-get install -y redis-server
+sudo apt-get install -y redis-server
 
 # setup rbenv and ruby-build
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
@@ -49,7 +35,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-# Install ruby 2.3.3 and bundler
+# Install ruby 2.1.4 and bundler
 export RBENV_ROOT="${HOME}/.rbenv"
 export PATH="${RBENV_ROOT}/bin:${PATH}"
 export PATH="${RBENV_ROOT}/shims:${PATH}"
@@ -64,17 +50,6 @@ rbenv rehash
 #sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
 #sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 #sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
-
-# app specifics
-cd /vagrant
-bundle install
-rbenv rehash
-#sudo sudo -u postgres psql -1 -c "CREATE USER pgadmin WITH PASSWORD 'secret';"
-#sudo sudo -u postgres psql -1 -c "ALTER USER pgadmin WITH SUPERUSER;"
-#cp -R config/database.sample.yml config/database.yml
-#rake db:create
-#rake db:migrate
-#rake db:seed
 
 # cleanup
 sudo apt-get clean
